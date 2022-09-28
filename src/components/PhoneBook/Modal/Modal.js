d70863916fbf221fe0bfd87de3cdfbd01b68d617
Maksym-Holovachuk <mgolovachuk@gmail.com>
2022-09-29 00:58:39 +0300

    fixed closure modal

--- src/components/PhoneBook/Modal/Modal.js
+++ src/components/PhoneBook/Modal/Modal.js
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types';
 import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { selectIsLoading } from 'redux/selectors';
 import { createPortal } from 'react-dom';
-
 import { ModalBackdrop, ModalContent } from './Modal.styled';
 import { IoIosClose } from 'react-icons/io';
 import ButtonIcon from 'components/PhoneBook/ButtonIcon';
@@ -15,20 +16,25 @@ export default function Modal({
   handleBackdropClick = () => {},
   closeModal = () => {},
 }) {
+  const isLoading = useSelector(selectIsLoading);
+
   useEffect(() => {
+    if (isLoading) {
+      return;
+    }
     window.addEventListener('keydown', handleKeyDown);
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, [handleKeyDown]);
+  }, [handleKeyDown, isLoading]);
 
   return createPortal(
-    <ModalBackdrop onClick={handleBackdropClick}>
+    <ModalBackdrop onClick={isLoading ? null : handleBackdropClick}>
       <ModalContent>
         <Box position="absolute" top="0" right="0" zIndex="100">
           <ButtonIcon
             type="button"
-            onClick={closeModal}
+            onClick={isLoading ? null : closeModal}
             aria-label="close modal"
           >
             <IoIosClose size="40px" />
